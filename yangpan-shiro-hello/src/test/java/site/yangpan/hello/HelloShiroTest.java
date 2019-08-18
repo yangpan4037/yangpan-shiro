d0319e60b489eadb4b80c404723d8629498e79a2
yangpan <916780135@qq.com>
2019-08-18 18:48:15 +0800

    初识及简介

---
+++ yangpan-shiro-hello/src/test/java/site/yangpan/hello/HelloShiroTest.java
@@ -0,0 +1,45 @@
+package site.yangpan.hello;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * HelloShiroTest
+ * Created by yangpan on 2019-08-18 11:43.
+ */
+public class HelloShiroTest {
+
+    @Test
+    public void testHello(){
+        //1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
+        Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+
+        //2、得到SecurityManager实例 并绑定给SecurityUtils
+        SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+
+        //3、得到Subject及创建用户名/密码身份验证Token（即用户身份/凭证）
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("yangpan", "123456");
+        try {
+            //4、登录，即身份验证
+            subject.login(token);
+        } catch (AuthenticationException e) {
+            //5、身份验证失败
+            e.printStackTrace();
+        }
+
+        //断言用户已经登录
+        Assert.assertEquals(true, subject.isAuthenticated());
+
+        //6、退出
+        subject.logout();
+    }
+}
